168ad65a6f25b818758881b865372157bdb97f1c
mewxz029 <mewlawbreakertv@gmail.com>
2024-11-10 19:38:15 +0700

    maps

---
+++ maps/go.mod
@@ -0,0 +1,3 @@
+module github.com/mewxz029/maps
+
+go 1.23.3
---
+++ maps/main.go
@@ -0,0 +1,56 @@
+package main
+
+type Dictionary map[string]string
+
+const (
+	ErrorNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrorWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrorWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrorNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrorWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		return ErrorWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
