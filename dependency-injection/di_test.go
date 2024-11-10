5bfb78b2eb94c4cb2bf6c3efea05b14770a80580
mewxz029 <mewlawbreakertv@gmail.com>
2024-11-10 20:10:02 +0700

    dependency injection

---
+++ dependency-injection/di_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+	got := buffer.String()
+	want := "Hello, Chris"
+	assertCorrectMessage(t, got, want)
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
---
+++ dependency-injection/go.mod
@@ -0,0 +1,3 @@
+module github.com/mewxz029/tdd/dependency-injection
+
+go 1.23.3
